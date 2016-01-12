9e369901be6cd66d02900d3259baa6c070713a62
Cam McKasson <cam@mckassons.com>
2016-01-11 19:33:25 -0800

    working on the camera...
    ...

---
+++ Assets/CamControl.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+    public GameObject player;
+
+    public float fDist;
+
+    private Vector3 viewLine;
+    private float h;
+    private float v;
+
+    
+    private Transform temp;
+
+    // Use this for initialization
+    void Start () {
+
+        viewLine = transform.position - player.transform.position;
+        viewLine = viewLine.normalized;
+
+        transform.position = player.transform.position + viewLine * fDist;
+
+        h = 0.0f;
+        v = 0.0f;	
+	}
+	
+    void getInput() {
+
+        h = Input.GetAxis( "rHorizontal" ) * 180.0f;
+        v = Input.GetAxis( "rVertical" ) * 180.0f;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        getInput();
+	}
+
+
+    // LateUpdate is called once per frame after Update
+    void LateUpdate() {
+
+        //viewLine = transform.position - player.transform.position;
+
+        //transform.position = player.transform.position;
+
+        v = Mathf.Clamp(v, -120.0f, 120.0f);
+
+        Quaternion temp = Quaternion.Euler(v, h, 0.0f);
+
+        transform.position = temp * ( new Vector3(0.0f, 0.0f, -fDist) ) + player.transform.position;
+        transform.rotation = temp;
+    }
+}
+
+/*
+Note to self
+
+    The camera can be positioned anywhere (respecting planes of symetry for camera rotation)
+
+    Now I need some trick to make the controls behave the way I want....
+*/
---
+++ Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        winText.text = "";
+        setText();
+    }
+
+    // FixedUpdate is called once per timestep
+    void FixedUpdate()
+    {
+
+        float moveH = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 move = new Vector3(moveH, 0.0f, moveV);
+        move = move.normalized;
+        move *= speed;
+
+        rb.AddForce(move);
+
+        if (rb.position.y <= -5.0f)
+        {
+
+            winText.text = ":(";
+            StartCoroutine(resetDelay());
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+
+            other.gameObject.SetActive(false);
+            count++;
+            setText();
+        }
+    }
+
+    void setText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+        {
+
+            winText.text = "You Win!";
+
+            StartCoroutine(EndDelay());
+        }
+    }
+
+    IEnumerator EndDelay()
+    {
+
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator resetDelay()
+    {
+
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
