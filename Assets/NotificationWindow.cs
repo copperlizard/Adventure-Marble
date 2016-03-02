035d8cfc5b3d255b5ada81ac800f0dd3a1344c51
Cam McKasson <cam@mckassons.com>
2016-03-01 17:47:00 -0800

    still working on notification window

---
+++ Assets/NotificationWindow.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotificationWindow : MonoBehaviour
+{
+    private Animator ani;
+    private CanvasGroup cgroup;
+    private string playername;
+    private bool shouldOpen;
+
+    IEnumerator checkDelay()
+    {
+        //DOESN'T STOP UNTIL OBJECT DESTORYED
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            playername = PlayerPrefs.GetString("PlayerName", "");
+            if (playername == "")
+            {
+                Debug.Log("shouldOpen == true");
+                shouldOpen = true;                
+            }
+            else
+            {
+                Debug.Log("shouldOpen == false");
+                shouldOpen = false;
+            }            
+        }        
+    }
+
+    public bool isOpen
+    {
+        get { return ani.GetBool("isOpen"); }
+        set { ani.SetBool("isOpen", value); shouldOpen = value; }
+    }    
+
+    // Called when loaded
+    public void awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        cgroup = GetComponentInChildren<CanvasGroup>();
+
+        //Reset canvas position to screen center
+        RectTransform rect = GetComponent<RectTransform>();
+        rect.offsetMax = Vector2.zero;
+        rect.offsetMin = Vector2.zero;
+
+        playername = PlayerPrefs.GetString("PlayerName", "");
+
+        if (playername == "")
+        {
+            shouldOpen = true;
+        }
+        else
+        {
+            shouldOpen = false;
+        }
+        StartCoroutine(checkDelay());
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {        
+        if (shouldOpen && !isOpen)
+        {
+            isOpen = true;
+            Debug.Log("isOpen == true");
+        }
+        else if(!shouldOpen && isOpen)
+        {
+            isOpen = false;
+            Debug.Log("isOpen == false");
+        }
+        
+
+        if (!ani.GetCurrentAnimatorStateInfo(0).IsName("open"))
+        {
+            cgroup.blocksRaycasts = false;
+            cgroup.interactable = false;
+        }
+        else
+        {
+            cgroup.blocksRaycasts = true;
+            cgroup.interactable = true;
+        }
+    }
+}
--- Assets/_Scripts/SaveFieldInput.cs
+++ Assets/_Scripts/SaveFieldInput.cs
@@ -5,11 +5,27 @@ using System.Collections;
 public class SaveFieldInput : MonoBehaviour
 {
     private InputField inField;
+    private string curName;
 
-	// Use this for initialization
-	void Start ()
+    IEnumerator checkDelay()
     {
-        inField = gameObject.GetComponent<InputField>();	
+        //DOESN'T STOP UNTIL OBJECT DESTORYED
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            curName = PlayerPrefs.GetString("PlayerName", "");
+            if (curName != inField.text)
+            {
+                //...
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        inField = gameObject.GetComponent<InputField>();
+        StartCoroutine(checkDelay());	
 	}
 	
 	// Update is called once per frame
