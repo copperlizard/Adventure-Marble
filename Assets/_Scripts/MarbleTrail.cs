343f711bf313b1a3df7d1c8e073291fc5a144256
Cam McKasson <cam@mckassons.com>
2016-03-08 18:05:30 -0800

    changed skys, added marble trail

---
+++ Assets/_Scripts/MarbleTrail.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarbleTrail : MonoBehaviour
+{
+    void Awake ()
+    {
+        int state = PlayerPrefs.GetInt("MarbleTrail", 0);
+
+        if (state == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {       
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}    
+}
---
+++ Assets/_Scripts/ToggleMarbleTrail.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleMarbleTrail : MonoBehaviour
+{
+    private bool active;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if(PlayerPrefs.GetInt("MarbleTrail", 0) == 1)
+        {
+            GetComponent<Toggle>().isOn = true;
+            active = true;
+        }
+        else
+        {
+            GetComponent<Toggle>().isOn = false;
+            active = false;
+        }	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void toggleTrailState()
+    {
+        if (!active)
+        {
+            PlayerPrefs.SetInt("MarbleTrail", 1);
+            GetComponent<Toggle>().isOn = true;
+            active = true;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("MarbleTrail", 0);
+            GetComponent<Toggle>().isOn = false;
+            active = false;
+        }
+    }
+}
